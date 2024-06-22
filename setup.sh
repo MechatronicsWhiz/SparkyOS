7bd3a0e234dc3224b9000b877294c2a32499d383
Bo <bocalpoly@gmail.com>
2024-06-21 20:37:18 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,14 +3,14 @@
 # Function to reboot and indicate the script phase
 reboot_system() {
     phase=$1
-    echo $phase > /tmp/script_phase
+    echo $phase > /var/tmp/script_phase
     echo "System will reboot now to continue with phase $phase."
     sudo reboot
 }
 
 # Read the current phase
-if [ -f /tmp/script_phase ]; then
-    phase=$(cat /tmp/script_phase)
+if [ -f /var/tmp/script_phase ]; then
+    phase=$(cat /var/tmp/script_phase)
 else
     phase="start"
 fi
@@ -36,18 +36,18 @@ case $phase in
         
     "addingPackages")
         # Phase 3: Install additional packages and configure autologin
-        sudo apt install chromium thonny python3-pyqt5 python3-pyqt5.qtwebengine -y
+        sudo apt install chromium-browser thonny python3-pyqt5 python3-pyqt5.qtwebengine -y
         sudo raspi-config nonint do_boot_behaviour B4
         
         # Cleanup and final reboot
-        rm /tmp/script_phase
+        rm /var/tmp/script_phase
         echo "Setup is complete. The system will reboot one last time."
         sudo reboot
         ;;
         
     *)
         echo "Unknown phase: $phase. Starting from the beginning."
-        rm -f /tmp/script_phase
+        rm -f /var/tmp/script_phase
         exec $0
         ;;
 esac
