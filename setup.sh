84fe53c24aca506650c7b4748019cc5d51ecedc0
Bo <bocalpoly@gmail.com>
2024-06-21 20:13:46 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,15 +4,17 @@
 sudo apt update
 sudo apt upgrade -y
 
-# Install LXQt, GVFS, Openbox, LightDM (without recommended packages)
+# Install LXQt, GVFS (without recommended packages)
 sudo apt --no-install-recommends install lxqt-core gvfs -y
+sudo reboot
+# Install Openbox, LightDM 
 sudo apt openbox lightdm -y
+sudo reboot
 
 # Install additional packages
 sudo apt install chromium-browser thonny python3-pyqt5 python3-pyqt5.qtwebengine -y
 
 # Configure autologin for the 'pi' user
 sudo raspi-config nonint do_boot_behaviour B4
-
 # Reboot to apply final changes
 sudo reboot
