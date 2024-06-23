63b239ddd2d7780af27ff46c23cbfa5fe6ce98be
Bo <bocalpoly@gmail.com>
2024-06-23 14:51:51 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,6 @@ sleep 2
 ################ Phase 2: Install desktop environment ################
 sudo apt-get --no-install-recommends install -y lxqt-core gvfs
 sudo apt-get install -y openbox lightdm
-sudo systemctl restart lightdm
 
 # Set lightdm to use autologin
 sudo raspi-config nonint do_boot_behaviour B4
