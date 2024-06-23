0972bfd23672ad7bef4aa11fd7850f25c83ab95d
Bo <bocalpoly@gmail.com>
2024-06-23 16:36:29 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,18 +16,27 @@ sleep 2
 
 
 ################ Phase 3: Install additional packages and configure autologin ################
-sudo apt-get install -y chromium-browser thonny python3-pyqt5 python3-pyqt5.qtwebengine
+# Install Thonny
+sudo apt-get install -y thonny
 
-sudo rpi-update -y 
-sudo apt remove python3-rpi.gpio -y 
-sudo pip3 install rpi-lgpio --upgrade RPi.GPIO --break-system-packages 
-sudo pip install SMBus rpi-ws281x --break-system-packages 
-sudo apt-get install gcc make build-essential python-dev-is-python3 scons swig python3-pil python3-pil.imagetk -y 
+# Install Chromium (Chromium is preferred over chromium-browser which may be deprecated in some systems)
+sudo apt-get install -y chromium
+
+# Install other required packages
+sudo apt-get install -y python3-pyqt5 python3-pyqt5.qtwebengine
+
+# Remove problematic packages and update rpi firmware
+sudo rpi-update -y
+sudo apt remove python3-rpi.gpio -y
+sudo pip3 install rpi-lgpio --upgrade RPi.GPIO --break-system-packages
+sudo pip install SMBus rpi-ws281x --break-system-packages
+
+# Install development tools and libraries
+sudo apt-get install -y gcc make build-essential python-dev-is-python3 scons swig python3-pil python3-pil.imagetk
 sudo apt install -y python3-opencv python3-numpy
 sudo apt install -y python3-scipy python3-matplotlib python3-joblib python3-opencv
-pip install scikit-learn --break-system-packages 
+pip install scikit-learn --break-system-packages
 python3 -m pip install mediapipe --break-system-packages
-
 
 echo "##################################################################"
 echo "########################## Phase 3 done ##########################"
