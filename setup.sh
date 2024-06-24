9f0fb9902ce0d2df8708141e2611579c4440d5f3
Bo <bocalpoly@gmail.com>
2024-06-23 22:05:46 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -29,13 +29,13 @@ sudo wget -O $GRAPH_DIR/$"settings_icon.png" $"https://raw.githubusercontent.com
 
 # Define an array of files to download and replace
 declare -a files=(
-    "rc.xml:$HOME/.config/openbox/rc.xml" # x
-    "lxqt.conf:$HOME/.config/lxqt/lxqt.conf" # x
-    #"lxqt-config-locale.conf:$HOME/.config/lxqt/lxqt-config-locale.conf"
-    "panel.conf:$HOME/.config/lxqt/panel.conf"
-    #"session.conf:$HOME/.config/lxqt/session.conf"
-    "settings.conf:$HOME/.config/pcmanfm-qt/lxqt/settings.conf"
-    #"01_debian.conf:/usr/share/lightdm/lightdm-gtk-greeter.conf.d/01_debian.conf"
+    # "rc.xml:$HOME/.config/openbox/rc.xml" # x
+    # "lxqt.conf:$HOME/.config/lxqt/lxqt.conf" # x
+    "lxqt-config-locale.conf:$HOME/.config/lxqt/lxqt-config-locale.conf"
+    #"panel.conf:$HOME/.config/lxqt/panel.conf"
+    "session.conf:$HOME/.config/lxqt/session.conf"
+    #"settings.conf:$HOME/.config/pcmanfm-qt/lxqt/settings.conf"
+    "01_debian.conf:/usr/share/lightdm/lightdm-gtk-greeter.conf.d/01_debian.conf"
 )
 
 # GitHub repository URL
