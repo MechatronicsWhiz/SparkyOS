b03220943163d3eedda1f432314ff5a3cc182d02
Bo <bocalpoly@gmail.com>
2024-06-23 23:02:11 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,3 @@
-#!/bin/bash
-################ Phase 1: Update and install LXQt, GVFS ################
-sudo apt-get update
-sudo apt-get upgrade -y
-echo "##################################################################"
-echo "########################## Phase 1 done ##########################"
-sleep 2
-
-################ Phase 2: Install desktop environment ################
-sudo apt-get --no-install-recommends install -y lxqt-core gvfs
-sudo apt-get install -y openbox lightdm
-
-echo "##################################################################"
-echo "########################## Phase 2 done ##########################"
-sleep 2
-
-
 ################ Phase 3: Configure the desktop for LXQt ################
 # Remove images
 WALLPAPER_DIR="/usr/share/lxqt/wallpapers"
@@ -37,7 +20,7 @@ declare -A config_dir=(
     #["$HOME/.config/lxqt/lxqt-config-locale.conf"]="lxqt-config-locale.conf"
     #["$HOME/.config/lxqt/panel.conf"]="panel.conf"
     ["$HOME/.config/lxqt/session.conf"]="session.conf"
-    #["$HOME/.config/pcmanfm-qt/lxqt/settings.conf"]="settings.conf"
+    ["$HOME/.config/pcmanfm-qt/lxqt/settings.conf"]="settings.conf"
     #["/media/pi/rootfs/usr/share/lightdm/lightdm-gtk-greeter.conf.d/01_debian.conf"]="01_debian.conf"
 )
 
@@ -54,38 +37,3 @@ done
 echo "##################################################################"
 echo "########################## Phase 3 done ##########################"
 sleep 2
-
-################ Phase 4: Install additional packages and configure autologin ################
-# Install Thonny
-sudo apt-get install -y thonny
-
-# Install Chromium (Chromium is preferred over chromium-browser which may be deprecated in some systems)
-sudo apt-get install -y chromium
-
-# Install other required packages
-# sudo apt-get install -y python3-pyqt5 python3-pyqt5.qtwebengine
-
-# Remove problematic packages and update rpi firmware
-# sudo rpi-update -y
-# sudo apt remove python3-rpi.gpio -y
-# sudo pip3 install rpi-lgpio --upgrade RPi.GPIO --break-system-packages
-# sudo pip install SMBus rpi-ws281x --break-system-packages
-
-# Install development tools and libraries
-# sudo apt-get install -y gcc make build-essential python-dev-is-python3 scons swig python3-pil python3-pil.imagetk
-# sudo apt install -y python3-opencv python3-numpy
-# sudo apt install -y python3-scipy python3-matplotlib python3-joblib python3-opencv
-# pip install scikit-learn --break-system-packages
-# python3 -m pip install mediapipe --break-system-packages
-
-echo "##################################################################"
-echo "########################## Phase 4 done ##########################"
-sleep 2
-
-################ Reboot ################
-# Enable the autologin service
-sudo raspi-config nonint do_boot_behaviour B4 # Set lightdm to use autologin
-sudo systemctl enable lightdm.service
-
-echo "System update and setup completed successfully. Rebooting..."
-sudo reboot
