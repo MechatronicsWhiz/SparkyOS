765ccb4c7aa9512cf82c737b1ad088fc348ad212
Bo <bocalpoly@gmail.com>
2024-06-23 22:18:49 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,69 +15,7 @@ echo "########################## Phase 2 done ##########################"
 sleep 2
 
 
-################ Phase 3: Configure the desktop for LXQt ################
-# Remove images
-WALLPAPER_DIR="/usr/share/lxqt/wallpapers"
-GRAPH_DIR="/usr/share/lxqt/graphics"
-sudo rm -f $WALLPAPER_DIR/*
-sudo rm -f $GRAPH_DIR/*
 
-# Download the new image from GitHub
-sudo wget -O $WALLPAPER_DIR/$"wallpaper1.svg" $"https://raw.githubusercontent.com/MechatronicsWhiz/sparkyos/main/resources/wallpaper1.svg"
-sudo wget -O $WALLPAPER_DIR/$"wallpaper2.png" $"https://raw.githubusercontent.com/MechatronicsWhiz/sparkyos/main/resources/wallpaper2.png"
-sudo wget -O $GRAPH_DIR/$"settings_icon.png" $"https://raw.githubusercontent.com/MechatronicsWhiz/sparkyos/main/resources/settings_icon.png"
-
-# Define an array of files to download and replace
-declare -a files=(
-    # "rc.xml:$HOME/.config/openbox/rc.xml" # x
-    # "lxqt.conf:$HOME/.config/lxqt/lxqt.conf" # x
-    "lxqt-config-locale.conf:$HOME/.config/lxqt/lxqt-config-locale.conf"
-    #"panel.conf:$HOME/.config/lxqt/panel.conf"
-    "session.conf:$HOME/.config/lxqt/session.conf"
-    #"settings.conf:$HOME/.config/pcmanfm-qt/lxqt/settings.conf"
-    "01_debian.conf:/usr/share/lightdm/lightdm-gtk-greeter.conf.d/01_debian.conf"
-)
-
-# GitHub repository URL
-CONFIG_DIR="https://raw.githubusercontent.com/MechatronicsWhiz/sparkyos/main/configration/"
-
-# Function to download files
-download_file() {
-    local filename=$1
-    local local_path=$2
-    local config_url="${CONFIG_DIR}${filename}"
-    local dir_path=$(dirname "${local_path}")
-
-    # Create directory if it doesn't exist
-    if [ ! -d "${dir_path}" ]; then
-        sudo mkdir -p "${dir_path}"
-        if [ $? -eq 0 ]; then
-            echo "Created directory ${dir_path}"
-        else
-            echo "Failed to create directory ${dir_path}"
-            exit 1
-        fi
-    fi
-
-    # Download the file
-    sudo wget -q --show-progress --no-check-certificate -O "${local_path}" "${config_url}"
-    if [ $? -eq 0 ]; then
-        echo "Successfully downloaded ${filename} to ${local_path}"
-    else
-        echo "Failed to download ${filename}"
-        exit 1
-    fi
-}
-
-# Download each file from GitHub
-for entry in "${files[@]}"; do
-    IFS=':' read -ra file <<< "$entry"
-    download_file "${file[0]}" "${file[1]}"
-done
-
-echo "##################################################################"
-echo "########################## Phase 3 done ##########################"
-sleep 2
 
 
 ################ Phase 4: Install additional packages and configure autologin ################
