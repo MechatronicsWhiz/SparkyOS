f3c28faaf53ca01c69704a67167c8b6d3a37d82e
Bo <bocalpoly@gmail.com>
2024-06-25 10:21:50 -0700

    Update and rename upgrade.sh to update_upgrade.sh

--- update_upgrade.sh
+++ update_upgrade.sh
@@ -2,7 +2,16 @@
 ################ Phase 1: Update and upgrade ################
 sudo apt-get update
 sudo apt-get upgrade -y
+
 echo "##################################################################"
 echo "########################## Phase 1 done ##########################"
 sleep 2
+
+script1_path="$HOME/update_upgrade.sh"
+script2_path="$HOME/install_desktop.sh"
+
+# Add script2 to crontab and remove script1
+(crontab -l 2>/dev/null; echo "@reboot $script2_path") | crontab -
+(crontab -l | grep -v "@reboot $script1_path") | crontab -
+
 sudo reboot
