3cb2b734b917582344c3ba419ef8b65cafb6699b
Bo <bocalpoly@gmail.com>
2024-06-25 10:27:13 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,21 +9,13 @@ sudo chmod +x $HOME/update_upgrade.sh
 sudo chmod +x $HOME/install_desktop.sh
 sudo chmod +x $HOME/config_desktop.sh
 
-# 2. Add a script to crontab
-add_to_crontab() {
-    local script_path=$1
-    (crontab -l 2>/dev/null; echo "@reboot $script_path") | crontab -
-}
-
 
 echo "##################################################################"
 echo "############################ Starting ############################"
 sleep 5
 
-# 3. Add script1 to crontab
+# 2. Add script1 to crontab
 script1_path="$HOME/update_upgrade.sh" 
-
-# Add script1 to crontab
-add_to_crontab $script1_path
+(crontab -l 2>/dev/null; echo "@reboot $script1_path") | crontab -
 
 sudo reboot
