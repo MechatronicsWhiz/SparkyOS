3480e43d577db35d8dc9d2e0d5bbd49090857df7
Bo <bocalpoly@gmail.com>
2024-06-25 11:08:57 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
 # 1. Download shell scripts
-sudo wget "https://raw.githubusercontent.com/MechatronicsWhiz/SparkyOS/main/update_upgrade.sh" -O $HOME/update_upgrade.sh
 sudo wget "https://raw.githubusercontent.com/MechatronicsWhiz/SparkyOS/main/install_desktop.sh" -O $HOME/install_desktop.sh
 sudo wget "https://raw.githubusercontent.com/MechatronicsWhiz/SparkyOS/main/config_desktop.sh" -O $HOME/config_desktop.sh
 
-sudo chmod +x $HOME/update_upgrade.sh
 sudo chmod +x $HOME/install_desktop.sh
 sudo chmod +x $HOME/config_desktop.sh
 
+# 2. Execute the first script
+./install_desktop.sh
+if [ $? -ne 0 ]; then
+  echo "script1.sh failed"
+  exit 1
+fi
 
-echo "##################################################################"
-echo "############################ Starting ############################"
-sleep 5
+# Execute the second script
+./config_desktop.sh
+if [ $? -ne 0 ]; then
+  echo "script2.sh failed"
+  exit 1
+fi
 
-# 2. Add script1 to crontab
-script1_path="$HOME/update_upgrade.sh" 
-(crontab -l 2>/dev/null; echo "@reboot $script1_path") | crontab -
+echo "###########################################################################"
+echo "############################ Installation done ############################"
+sleep 2
 
+sudo rm *
 sudo reboot
