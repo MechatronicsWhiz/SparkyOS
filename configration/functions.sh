87b64f2ef130b5e0648f386551e337bf374aa60b
Bo <bocalpoly@gmail.com>
2024-06-28 21:22:20 -0700

    Update functions.sh

--- configration/functions.sh
+++ configration/functions.sh
@@ -41,55 +41,9 @@ add_app_to_panel() {
     echo "App $new_entry installed"
 }
 
-# Function to update system packages
-update_system() {
-    sudo apt update && sudo apt upgrade -y
-    
-    echo "************************* System Updated *************************"
-    sleep 2
-}
-
 # Function to clean up temporary files
 cleanup_temp_files() {
     sudo rm -rf /tmp/*
     echo "Temporary files cleaned up."
-}
-
-# Function to install desktop
-install_desktop(){
-  sudo apt-get --no-install-recommends install -y lxqt-core gvfs
-  sudo apt-get install -y openbox lightdm #
-  sudo raspi-config nonint do_boot_behaviour B4 # Set lightdm to use autologin
-  sudo systemctl enable lightdm.service
-  
-  echo "************************* Desktop installed *************************"
-  sleep 2
-}
-
-# Function to install packages
-install_packages(){
-  sudo apt-get install -y thonny   # Install Thonny
-  sudo apt-get install -y chromium
-  sudo apt-get install -y python3-pyqt5 python3-pyqt5.qtwebengine
-  
-  # Remove problematic packages and update rpi firmware
-  # sudo rpi-update -y
-  # sudo apt remove python3-rpi.gpio -y
-  # sudo pip3 install rpi-lgpio --upgrade RPi.GPIO --break-system-packages
-  # sudo pip install SMBus rpi-ws281x --break-system-packages
-  
-  # Install development tools and libraries
-  # sudo apt-get install -y gcc make build-essential python-dev-is-python3 scons swig python3-pil python3-pil.imagetk
-  # sudo apt install -y python3-opencv python3-numpy
-  # sudo apt install -y python3-scipy python3-matplotlib python3-joblib python3-opencv
-  # pip install scikit-learn --break-system-packages
-  # python3 -m pip install mediapipe --break-system-packages
-  
-  # Enable the autologin service
-  # sudo raspi-config nonint do_boot_behaviour B4 # Set lightdm to use autologin
-  # sudo systemctl enable lightdm.service
-  
-  echo "************************* Packages installed *************************"
-  sleep 2
 }
 
