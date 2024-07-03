ee5fd1dd5edae141fa8fc0502962d9202fb6a60e
Bo <bocalpoly@gmail.com>
2024-07-02 21:25:30 -0700

    Update test.sh

--- test1.sh
+++ test1.sh
@@ -5,20 +5,22 @@ sudo apt update
 sudo apt upgrade -y
 
 # Install LXQt, GVFS, Sway, and LightDM
-sudo apt-get --no-install-recommends install -y lxqt-core gvfs sway lightdm
+sudo apt-get --no-install-recommends install -y lxqt-core gvfs
+sudo apt-get install -y lightdm sway
 
-# Determine current username
-CURRENT_USER=$(whoami)
-
-# Configure LightDM for automatic login with the current username
 # Enable the autologin service
 sudo raspi-config nonint do_boot_behaviour B4 # Set lightdm to use autologin
 sudo systemctl enable lightdm.service
 
+# Determine current username
+CURRENT_USER=$(whoami)
+
+# Edit LightDM configuration for autologin
+sudo bash -c "cat <<EOF >/etc/lightdm/lightdm.conf
 [Seat:*]
 autologin-user=$CURRENT_USER
 autologin-session=sway
-EOF
+EOF"
 
 # Reboot to apply changes
 sudo reboot
