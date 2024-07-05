6428c16124c78db2301ee44360e50d52d5148cad
Bo <bocalpoly@gmail.com>
2024-07-04 17:57:31 -0700

    Update install_desktop.sh

--- install_desktop.sh
+++ install_desktop.sh
@@ -7,6 +7,11 @@ sudo apt upgrade -y
 sudo apt-get --no-install-recommends install -y lxqt-core gvfs
 sudo apt-get install -y openbox lightdm #
 
+# Install Thonny and Chrome
+sudo apt-get install -y thonny
+sudo apt-get install -y chromium
+
+# Auto login
 sudo raspi-config nonint do_boot_behaviour B4 # Set lightdm to use autologin
 sudo systemctl enable lightdm.service
 
