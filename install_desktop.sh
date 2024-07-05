3ed39972979d150302d5fc31a6f34eb010d4967b
Bo <bocalpoly@gmail.com>
2024-07-04 18:36:54 -0700

    Update install_desktop.sh

--- install_desktop.sh
+++ install_desktop.sh
@@ -7,6 +7,8 @@ sudo apt upgrade -y
 sudo apt-get --no-install-recommends install -y lxqt-core gvfs
 sudo apt-get install -y openbox lightdm #
 
+sudo apt-get install -y thonny
+sudo apt-get install -y chromium
 
 # Auto login
 sudo raspi-config nonint do_boot_behaviour B4 # Set lightdm to use autologin
