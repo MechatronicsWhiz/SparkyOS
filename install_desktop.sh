815d8045abb401f3b777d24ee7d45ad2023fe8ac
Bo <bocalpoly@gmail.com>
2024-07-04 19:04:57 -0700

    Update install_desktop.sh

--- install_desktop.sh
+++ install_desktop.sh
@@ -7,15 +7,22 @@ sudo apt upgrade -y
 sudo apt-get --no-install-recommends install -y lxqt-core gvfs
 sudo apt-get install -y openbox lightdm #
 
+
+################ Phase 3: Install additional packages and configure autologin ################
+# Install Thonny
 sudo apt-get install -y thonny
+
+# Install Chromium (Chromium is preferred over chromium-browser which may be deprecated in some systems)
 sudo apt-get install -y chromium
 
-# Auto login
+# Install other required packages
+sudo apt-get install -y python3-pyqt5 python3-pyqt5.qtwebengine
+
+
+# Enable the autologin service
 sudo raspi-config nonint do_boot_behaviour B4 # Set lightdm to use autologin
 sudo systemctl enable lightdm.service
 
 echo "##################################################################"
 echo "########################## Phase 1 done ##########################"
 sleep 2
-
-
