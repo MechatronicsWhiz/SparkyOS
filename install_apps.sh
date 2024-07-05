5abdca782b5df479a9d94e1584a54a002480e2d9
Bo <bocalpoly@gmail.com>
2024-07-04 20:29:05 -0700

    Update install_apps.sh

--- install_apps.sh
+++ install_apps.sh
@@ -43,4 +43,34 @@ app_builder_download=(
     "app_builder.py"
     "app_builder_icon.png"
 )
+
+
+################ Phase 2: Install additional packages and configure autologin ################
 download_files "$app_builder_url" "${app_builder_download[@]}"
+
+
+# Install Thonny
+sudo apt-get install -y thonny
+
+# Install Chromium (Chromium is preferred over chromium-browser which may be deprecated in some systems)
+sudo apt-get install -y chromium
+
+# Install other required packages
+sudo apt-get install -y python3-pyqt5 python3-pyqt5.qtwebengine
+
+# Remove problematic packages and update rpi firmware
+sudo rpi-update -y
+sudo apt remove python3-rpi.gpio -y
+sudo pip3 install rpi-lgpio --upgrade RPi.GPIO --break-system-packages
+sudo pip install SMBus rpi-ws281x --break-system-packages
+
+# Install development tools and libraries
+sudo apt-get install -y gcc make build-essential python-dev-is-python3 scons swig python3-pil python3-pil.imagetk
+sudo apt install -y python3-opencv python3-numpy
+sudo apt install -y python3-scipy python3-matplotlib python3-joblib python3-opencv
+pip install scikit-learn --break-system-packages
+python3 -m pip install mediapipe --break-system-packages
+
+echo "##################################################################"
+echo "########################## Phase 2 done ##########################"
+sleep 2
