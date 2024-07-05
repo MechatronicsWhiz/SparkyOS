3aa4173d3af29456013b3b9ff30326231451d809
Bo <bocalpoly@gmail.com>
2024-07-04 19:28:47 -0700

    Update install_apps.sh

--- install_apps.sh
+++ install_apps.sh
@@ -43,28 +43,4 @@ app_builder_download=(
     "app_builder.py"
     "app_builder_icon.png"
 )
-
-# Install apps
 download_files "$app_builder_url" "${app_builder_download[@]}"
-
-
-################ Phase 3: Install additional packages and apps ################
-
-# Remove problematic packages and update rpi firmware
-# sudo rpi-update -y
-# sudo apt remove python3-rpi.gpio -y
-# sudo pip3 install rpi-lgpio --upgrade RPi.GPIO --break-system-packages
-# sudo pip install SMBus rpi-ws281x --break-system-packages
-
-# Install development tools and libraries
-# sudo apt-get install -y gcc make build-essential python-dev-is-python3 scons swig python3-pil python3-pil.imagetk
-# sudo apt install -y python3-opencv python3-numpy
-# sudo apt install -y python3-scipy python3-matplotlib python3-joblib python3-opencv
-# pip install scikit-learn --break-system-packages
-# python3 -m pip install mediapipe --break-system-packages
-
-
-
-echo "##################################################################"
-echo "########################## Phase 3 done ##########################"
-sleep 2
