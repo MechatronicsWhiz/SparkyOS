761033e8e0bc3d51f4cf3a4f2477631e1d678c7c
Bo <bocalpoly@gmail.com>
2024-08-05 15:59:20 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,42 +1,54 @@
 #!/bin/bash
 
 : <<'END'
-This it the master installation shell script for SparkyBotOS
+This is the master installation shell script for SparkyBotOS
 Comment out sections as needed.
 END
 
-# 1. Download shell scripts
-sudo wget "https://raw.githubusercontent.com/SparkyAutomation/SparkyOS/main/install_desktop.sh" -O $HOME/install_desktop.sh
-sudo wget "https://raw.githubusercontent.com/SparkyAutomation/SparkyOS/main/install_apps.sh" -O $HOME/install_apps.sh
-sudo wget "https://raw.githubusercontent.com/SparkyAutomation/SparkyOS/main/config_desktop.sh" -O $HOME/config_desktop.sh
+# Function to download and check success
+download_and_verify() {
+  local url=$1
+  local dest=$2
+  wget "$url" -O "$dest"
+  if [ $? -ne 0 ]; then
+    echo "Failed to download $url"
+    exit 1
+  fi
+  chmod +x "$dest"
+}
 
-sudo chmod +x $HOME/install_desktop.sh
-sudo chmod +x $HOME/install_apps.sh
-sudo chmod +x $HOME/config_desktop.sh
+# 1. Download shell scripts
+download_and_verify "https://raw.githubusercontent.com/SparkyAutomation/SparkyOS/main/install_desktop.sh" "$HOME/install_desktop.sh"
+download_and_verify "https://raw.githubusercontent.com/SparkyAutomation/SparkyOS/main/install_apps.sh" "$HOME/install_apps.sh"
+download_and_verify "https://raw.githubusercontent.com/SparkyAutomation/SparkyOS/main/config_desktop.sh" "$HOME/config_desktop.sh"
 
 # 2. Install desktop environment
-./install_desktop.sh
+$HOME/install_desktop.sh
 if [ $? -ne 0 ]; then
   echo "install_desktop.sh failed"
   exit 1
 fi
 
-# Install apps
-./install_apps.sh
+# 3. Install apps
+$HOME/install_apps.sh
 if [ $? -ne 0 ]; then
   echo "install_apps.sh failed"
   exit 1
 fi
 
-# Configure the OS
-./config_desktop.sh
+# 4. Configure the OS
+$HOME/config_desktop.sh
 if [ $? -ne 0 ]; then
   echo "config_desktop.sh failed"
   exit 1
 fi
+
 echo "###########################################################################"
 echo "############################ Installation done ############################"
 sleep 2
 
-sudo rm *
+# Clean up downloaded scripts
+rm -f $HOME/install_desktop.sh $HOME/install_apps.sh $HOME/config_desktop.sh
+
+# Reboot system
 sudo reboot
