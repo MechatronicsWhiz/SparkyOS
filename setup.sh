e4008c93f79e1229042e8073e22a53ba53e92239
Bo <bocalpoly@gmail.com>
2024-08-05 15:13:01 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ Comment out sections as needed.
 END
 
 # 1. Download shell scripts
-sudo wget "https://raw.githubusercontent.com/SparkyAutomation/SparkyOS/main/install_desktop.sh" -O $HOME/install_desktop.sh
-sudo wget "https://raw.githubusercontent.com/SparkyAutomation/SparkyOS/main/install_apps.sh" -O $HOME/install_apps.sh
-sudo wget "https://raw.githubusercontent.com/SparkyAutomation/SparkyOS/main/config_desktop.sh" -O $HOME/config_desktop.sh
+sudo wget "https://raw.githubusercontent.com/SparkyAutomation/SparkyOS/main/install_desktop.sh" -O install_desktop.sh
+sudo wget "https://raw.githubusercontent.com/SparkyAutomation/SparkyOS/main/install_apps.sh" -O install_apps.sh
+sudo wget "https://raw.githubusercontent.com/SparkyAutomation/SparkyOS/main/config_desktop.sh" -O config_desktop.sh
 
 sudo chmod +x $HOME/install_desktop.sh
 sudo chmod +x $HOME/install_apps.sh
